ed57d47c5ddb9b8db6c6929402649048570bc61f
Maurovic <Maurovic.cachia@gmail.com>
2022-02-06 00:34:55 +0100

    Initial Push

---
+++ match_day/lib/flavor_config.dart
@@ -0,0 +1,11 @@
+class FlavorConfig {
+  // final String apiBaseUrl;
+
+  // FlavorConfig.dev({
+  //   this.apiBaseUrl = "",
+  // });
+
+  // FlavorConfig.prod({
+  //   this.apiBaseUrl = "",
+  // });
+}
